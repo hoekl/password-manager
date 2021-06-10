d2b645ce41e62e3f5a1fd21576307d9c6a997f16
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-06-10 17:07:06 +0100

    Use secrets module for random choices & use string module as source for characters

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
+import secrets
+import string
 
 def pw_gen(pw_length):
-    char_choices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
-    pw = random.choices(char_choices, k=pw_length)
-    pw = ''.join(pw)
-    return(pw)
-pw_length = input("Specify the length of the password to generate: ")
-print(pw_gen(int(pw_length)))
+    password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pw_length)))
+    return(password)
 
+print(pw_gen(10))
