66dc68f979572ba1cec7bdd29ba5d090ff788add
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-06-11 17:37:46 +0100

    Link password generator to GUI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import secrets
 import string
 import wx
+from wx.core import Width
 
 class MainPanel(wx.Panel):
     def __init__(self, parent):
@@ -21,12 +22,21 @@ class PWGenWindow(wx.Dialog):
         super().__init__(parent=None, title='Generate new password')
         self.main_sizer = wx.BoxSizer(wx.VERTICAL)
         pwGenButton = wx.Button(self, label='Generate Password')
+        pwGenButton.Bind(wx.EVT_BUTTON, self.generatePW)
+        self.txt_ctrl = wx.TextCtrl(self, pos=(5, 5), size=(300, -1))
+        self.main_sizer.Add(self.txt_ctrl, wx.SizerFlags(0).Centre().Border(wx.ALL, 5))
         self.main_sizer.Add(pwGenButton, wx.SizerFlags().Centre().Border(wx.ALL, 5))
         self.SetSizer(self.main_sizer)
+
+    def generatePW(self, event):
+        password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(20)))
+        self.txt_ctrl.Clear()
+        self.txt_ctrl.write(password)
 class MainFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='Password Manager')
         self.panel = MainPanel(self)
+        self.CreateStatusBar()
 
 if __name__ == '__main__':
     app = wx.App()
