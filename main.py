91961fafdb079ced4c172c629884a077b48f90f6
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-06-11 15:56:39 +0100

    Implement panel, window, and button

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import secrets
 import string
+import wx
+
+class MainPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        pwGenButton = wx.Button(self, label='Generate Password')
+        pwGenButton.Bind(wx.EVT_BUTTON, self.onGenerate)
+        main_sizer.Add(pwGenButton, wx.SizerFlags().Centre().Border(wx.ALL, 5))
+        self.SetSizer(main_sizer)
+
+    def onGenerate():
+        pass
+
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Password Manager')
+        self.panel = MainPanel(self)
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MainFrame()
+    frame.Show()
+    app.MainLoop()
 
 def pw_gen(pw_length):
     password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pw_length)))
     return(password)
 
-print(pw_gen(10))
+#print(pw_gen(10))
 
