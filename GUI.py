5210558edfde2e89b0f27f23d4963336c0eccab1
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-06-25 15:54:18 +0100

    Implement select from listview, query from db and show result in textcontrol

--- GUI.py
+++ GUI.py
@@ -2,15 +2,18 @@ import wx
 import pprint
 from modules import login_creator as login
 from modules import db_manager as db
+
 pp = pprint.PrettyPrinter(indent=4)
 
-class BaseFrame(wx.Frame):
 
+class BaseFrame(wx.Frame):
     def __init__(self, *args, **kw):
         super(BaseFrame, self).__init__(*args, **kw)
 
         noteBookPanel = wx.Notebook(self)
-        firstPanel = login.MainPanel(noteBookPanel)     #implement own class to add functionality
+        firstPanel = login.MainPanel(
+            noteBookPanel
+        )  # implement own class to add functionality
         secondPanel = ListPanel(noteBookPanel)
         noteBookPanel.AddPage(firstPanel, "New Login", True)
         noteBookPanel.AddPage(secondPanel, "Logins", True)
@@ -19,43 +22,62 @@ class BaseFrame(wx.Frame):
         frameSizer.Add(firstPanel, wx.SizerFlags().Centre().Border(wx.ALL, 5))
         frameSizer.Add(secondPanel, wx.SizerFlags().Centre().Border(wx.ALL, 5))
 
-class ListPanel(wx.Panel):
 
+class ListPanel(wx.Panel):
     def __init__(self, *args, **kw):
         super(ListPanel, self).__init__(*args, **kw)
 
-        choices = self.getLoginsFromDB()
+        choicesList = self.getLoginsFromDB()
+        listBox = wx.ListBox(
+            self, size=(400, -1), choices=choicesList, style=wx.LB_SINGLE | wx.LB_SORT
+        )
 
-        listBox = wx.ListBox(self, size=(400, -1), choices = choices[1], style=wx.LB_SINGLE)
         self.textControl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
 
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(listBox, 0, wx.EXPAND)
         sizer.Add(self.textControl, 1, wx.EXPAND)
         self.SetSizer(sizer)
+
         self.Bind(wx.EVT_LISTBOX, self.onListBox, listBox)
 
+
+
     def onListBox(self, event):
-        self.textControl.AppendText("Current select:" + event.GetEventObject().GetStringSelection()+"\n")
+        self.textControl.Clear()
+        string = event.GetEventObject().GetStringSelection()
+        self.textControl.AppendText(
+            "Current select: " + string + "\n\n"
+        )
+        doc = db.getDocByID(self.linkDict, string)
+        listKeys = list(doc.keys())
+        listValues = list(doc.values())
+        listKeys.pop(0)
+        listKeys.pop(0)
+        listValues.pop(0)
+        listValues.pop(0)
+
+        for i in range(len(listKeys)):
+            strKey = str(listKeys[i])
+            strVal = str(listValues[i])
+            self.textControl.AppendText(strKey + ": " + strVal + "\n")
+
+
 
     def getLoginsFromDB(self):
         res = db.QueryAll()
-        id_list = []
-        site_list = []
+        self.linkDict = db.formatQueryRes(res)
+        choicesList = list(self.linkDict.keys())
 
-        for doc in res['docs']:
-            id = doc['_id']
-            website = doc['website']
-            id_list.append(id)
-            site_list.append(website)
-        return id_list, site_list
-        #pp.pprint(res)
-if __name__ == '__main__':
+        return choicesList
+        # pp.pprint(res)
+
+
+if __name__ == "__main__":
     # When this module is run (not imported) then create the app, the
     # frame, show it, and start the event loop.
     app = wx.App()
     db = db.DataBase("mock_data")
-    frm = BaseFrame(None, title='Password Manager', size=(1000,500))
+    frm = BaseFrame(None, title="Password Manager", size=(1000, 500))
     frm.Show()
     app.MainLoop()
-
--- modules/db_manager.py
+++ modules/db_manager.py
@@ -1,20 +1,22 @@
-import couchdb2
-import traceback
 import pprint
+import traceback
+
+import couchdb2
 
 pp = pprint.PrettyPrinter(indent=4)
 server = couchdb2.Server("http://admin:pwmanager@127.0.0.1:5984")
 
 
-class DataBase():
+class DataBase:
     def __init__(self, dbName) -> couchdb2.Database:
         self.name = dbName
         if server.up() == True:
             try:
-                self.db = server.get(self.name, check=True)     # raises NotFoundError if database doesn't exist
+                self.db = server.get(
+                    self.name, check=True
+                )  # raises NotFoundError if database doesn't exist
             except couchdb2.NotFoundError:
                 self.dbCreate()
-
 
     def Create(self):
         if server.up() == True:
@@ -23,50 +25,64 @@ class DataBase():
             except Exception:
                 print(traceback.print_exc())
 
-
     def buildQuerySelector(self, querytype, querystring):
         if querytype == "search":
             self.selector = {
-                "_id": {
-                    "$gt": None
-                },
-                "$or": [
-                    {"website": querystring},
-                    {"service name": querystring}
-                ]
+                "_id": {"$gt": None},
+                "$or": [{"website": querystring}, {"service name": querystring}],
             }
-
 
     def QueryAll(self):
         self.assertIsDB()
-        self.selector = {
-                "_id": {
-                    "$gt": None
-                }
-            }
+        self.selector = {"_id": {"$gt": None}}
 
         if self.db.exists() == True:
             res = self.db.find(self.selector, limit=1000)
-            #pp.pprint(res)
-
+            # pp.pprint(res)
         return res
-
 
     def Query(self):
         self.assertIsDB()
         assert self.selector != None
         if self.db.exists() == True:
             res = self.db.find(self.selector)
-            #pp.pprint(res)
+            # pp.pprint(res)
         return res
 
+    def formatQueryRes(self, res):
+        link_dict = {}
+        for doc in res["docs"]:
+            link_dict.update(
+                {
+                    doc["service name"]: {
+                        "id": doc["_id"],
+                        "website": doc["website"],
+                    }
+                }
+            )
+        #pp.pprint(link_dict)
+        return link_dict
 
+    def getIDfromDict(self, linkDict, serviceName):
+        doc = linkDict.get(serviceName)
+        uID = doc["id"]
+        return uID
+
+    def getDocByID(self, linkDict, serviceName):
+        uID = self.getIDfromDict(linkDict, serviceName)
+        doc = self.db.get(uID, rev=None, revs_info=False, conflicts=False)
+        pp.pprint(doc)
+        return doc
     def assertIsDB(self):
         try:
             assert isinstance(self.db, couchdb2.Database)
         except Exception:
-                print(traceback.print_exc())
+            print(traceback.print_exc())
+
 
 if __name__ == "__main__":
     db = DataBase("mock_data")
-
+    res = db.QueryAll()
+    link_dict = db.formatQueryRes(res)
+    uID = db.getIDfromDict(link_dict, "aliquam")
+    print(uID)
