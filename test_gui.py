adef08c3081acd84c406503fda937d9b5f016b25
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-07-28 15:32:09 +0100

    Add teardown method to fix bug when running certain tests sequentally. Also add testcase for password strength values

--- test_gui.py
+++ test_gui.py
@@ -1,19 +1,35 @@
 import unittest
-
-# from unittest.case import TestCase
 import time
 import wx
 import GUI as gui
 from modules.encryption_handler import CryptoKeyManager
 import lockout_manager
-from modules.custom_widgets import PWStrengthIndicator
+from modules.custom_widgets import PWStrengthIndicator, StrengthSizer
 
 
 class GUI_test(unittest.TestCase):
     def setUp(self) -> None:
         self.app = wx.App()
-        self.app.SetExitOnFrameDelete(True)
+        wx.Log.SetActiveTarget(wx.LogStderr())
+        self.frame = wx.Frame(None, title='WTC: '+self.__class__.__name__)
+        self.frame.Show()
+        self.frame.PostSizeEvent()
+
+    def tearDown(self):
+        def _cleanup():
+            for tlw in wx.GetTopLevelWindows():
+                if tlw:
+                    if isinstance(tlw, wx.Dialog) and tlw.IsModal():
+                        tlw.EndModal(0)
+                        wx.CallAfter(tlw.Destroy)
+                    else:
+                        tlw.Close(force=True)
+            wx.WakeUpIdle()
+
+        timer = wx.PyTimer(_cleanup)
+        timer.Start(100)
         self.app.MainLoop()
+        del self.app
 
     def test_correct_password(self):
         screen = gui.LoginScreen(None, "mock_data", "verification")
@@ -41,7 +57,61 @@ class GUI_test(unittest.TestCase):
         toc = time.perf_counter()
         length_t = toc - tic
         self.assertLess(length_t, 0.5)
-        wx.Exit()
+
+    def test_advice(self):
+        self.sizer = StrengthSizer(self.frame, c_style=None)
+        self.indicator = self.sizer.strengthbar
+        self.indicator.set_pw("aaaaaaaaaaaa")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using a longer password, uppercase characters, numbers and special characters for extra security.")
+        self.indicator.set_pw("aaaaaaaaaaaaa")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using uppercase characters, numbers and special characters for extra security.")
+        self.indicator.set_pw("aaaaaaaaaaaaA")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using numbers and special characters for extra security.")
+        self.indicator.set_pw("aaaaaaaaaaaA1")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using special characters for extra security.")
+        self.indicator.set_pw("aaaaaaaaaaaA1!")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "")
+        self.indicator.set_pw("aaaaaaaaaaa1!")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using uppercase characters for extra security.")
+        self.indicator.set_pw("aaaaaaaaaaaa!")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using uppercase characters and numbers for extra security.")
+        self.indicator.set_pw("AAAAAAAAAAAAA")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using lowercase characters, numbers and special characters for extra security.")
+        self.indicator.set_pw("1234567891011")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using lowercase characters, uppercase characters and special characters for extra security.")
+        self.indicator.set_pw("1234567891011!")
+        self.indicator.calc_strength()
+        self.assertEqual(self.indicator.advice, "Consider using lowercase characters and uppercase characters for extra security.")
+        self.indicator.set_pw(None)
+
+
+    def test_strength_calculator(self):
+        self.sizer = StrengthSizer(self.frame, c_style=None)
+        self.indicator = self.sizer.strengthbar
+        self.indicator.set_pw("£")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 1)
+        self.indicator.set_pw("aaaaaaaaaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 20)
+        self.indicator.set_pw("aaaaaaaaaaaa1")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 30)
+        self.indicator.set_pw("aaaaaaaaaaaa1!")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 40)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 50)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaaaaaaaaaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 70)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaaaaaaaaaa!!AAAAAA")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 90)
+        self.indicator.set_pw(None)
 
 
 class CryptoKeyTest(unittest.TestCase):
@@ -88,11 +158,12 @@ class LockoutTest(unittest.TestCase):
         time.sleep(30)
         self.assertEqual(self.lockout.check_pw("testing"), True)
         self.assertEqual(self.lockout.check_access(), True)
+        print("")
+
 
 class PasswordStrengthTest(unittest.TestCase):
     def setUp(self) -> None:
         self.app = wx.App()
-        self.app.SetExitOnFrameDelete(True)
         self.app.MainLoop()
         frame = wx.Frame()
         self.indicator = PWStrengthIndicator(frame, password=None, c_style=None)
@@ -129,6 +200,22 @@ class PasswordStrengthTest(unittest.TestCase):
         self.indicator.calc_strength()
         self.assertEqual(self.indicator.advice, "Consider using lowercase characters and uppercase characters for extra security.")
 
+    def test_strength_calculator(self):
+        self.indicator.set_pw("£")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 1)
+        self.indicator.set_pw("aaaaaaaaaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 20)
+        self.indicator.set_pw("aaaaaaaaaaaa1")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 30)
+        self.indicator.set_pw("aaaaaaaaaaaa1!")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 40)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 50)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaaaaaaaaaa")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 70)
+        self.indicator.set_pw("aaaaaaaaaaaa1!Aaaaaaaaaaa!!AAAAAA")
+        self.assertGreaterEqual(self.indicator.calc_strength(), 90)
+
 
 def suite():
     suite = unittest.TestSuite()
@@ -136,12 +223,13 @@ def suite():
     suite.addTest(GUI_test("test_incorrect_password"))
     suite.addTest(GUI_test("test_create_newdb"))
     suite.addTest(GUI_test("test_fetch_speed"))
+    suite.addTest(GUI_test("test_advice"))
+    suite.addTest(GUI_test("test_strength_calculator"))
     suite.addTest(CryptoKeyTest("test_salt"))
     suite.addTest(CryptoKeyTest("test_create_new_salt"))
     suite.addTest(LockoutTest("test_right_pw"))
     suite.addTest(LockoutTest("test_wrong_pw"))
     suite.addTest(LockoutTest("test_clear_lockout"))
-    suite.addTest(PasswordStrengthTest("test_advice"))
     return suite
 
 
